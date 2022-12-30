16e95dca15886b43bb1df0c6fd61038259465dc2
hudsonhrh <hudsonhrh7@gmail.com>
2022-12-30 16:46:57 -0600

    first

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,11 +1,12 @@
 import logo from './kubi logo.png';
+import jayhawk from './jayhawk-4.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} width ="600" height = "400" />
         <p>
           KU Blockcain Institute
         </p>
@@ -17,6 +18,7 @@ function App() {
         >
           KUBI Website
         </a>
+        <img src={jayhawk} width ="600" height = "600" />
       </header>
     </div>
   );
