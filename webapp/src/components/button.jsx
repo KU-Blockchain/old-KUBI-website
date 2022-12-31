66198afb6b7706f4411a115499e37aa82762e19e
Emma Nasseri <enasseri02@gmail.com>
2022-12-30 21:07:08 -0600

    updated favicon.ico

---
+++ webapp/src/components/button.jsx
@@ -0,0 +1,11 @@
+import Button from 'react-bootstrap/Button';
+
+function ButtonComponent() {
+  return (
+    <>
+      <Button variant="primary">Primary</Button>{' '}
+    </>
+  );
+}
+
+export default ButtonComponent;
