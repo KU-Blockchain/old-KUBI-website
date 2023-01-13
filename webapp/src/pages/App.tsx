6b2aedfa66ba9e2c88160684a7dd4f3809c8fc92
Emma Nasseri <enasseri02@gmail.com>
2023-01-12 18:40:16 -0600

    edited tab image and title

--- webapp/src/pages/App.tsx
+++ webapp/src/pages/App.tsx
@@ -10,34 +10,9 @@ const link = [{ link: "./DAO", label: "DAO" }];
 function App() {
   return (
     <MantineProvider withGlobalStyles withNormalizeCSS>
-      <>
+      
         <HeaderSimple links={link}></HeaderSimple>
 
-        <div className="App">
-          <nav>
-            <ul>
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-              <li>
-                <Link to="DAO">DAO</Link>
-              </li>
-              <li>
-                <Link to="about">About</Link>
-              </li>
-            </ul>
-          </nav>
-          <div className="main">
-            {/* Define all the routes */}
-            <Routes>
-              <Route path="/" element={<Home />}></Route>
-              <Route path="about" element={<About />}></Route>
-              <Route path="DAO" element={<DAO />}></Route>
-              <Route path="*" element={<NotFound />}></Route>
-            </Routes>
-          </div>
-        </div>
-      </>
     </MantineProvider>
   );
 }
