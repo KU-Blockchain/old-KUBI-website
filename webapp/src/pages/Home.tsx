33cea1670cc0fb5567fb3fe937ba7b426356c200
Emma Nasseri <enasseri02@gmail.com>
2023-01-12 20:01:57 -0600

    added carousel

--- webapp/src/pages/Home.tsx
+++ webapp/src/pages/Home.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import { Carousel } from "@mantine/carousel";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <>
+      <div>Home</div>;
+      <Carousel></Carousel>
+    </>
+  );
 };
 
 export default Home;
