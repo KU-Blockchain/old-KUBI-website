32d8e6bf6cd3f96be07160000ac0adda1f59162c
Emma Nasseri <enasseri02@gmail.com>
2023-01-12 19:51:55 -0600

    added multiple bio cards

--- webapp/src/pages/About.tsx
+++ webapp/src/pages/About.tsx
@@ -5,7 +5,42 @@ const About = () => {
   return (
     <>
       <div>About</div>
-      <BioCard avatar={'avatar'} name={"Emma Nasseri"} email="enasseri02@ku.edu" job={'President'}></BioCard>
+      <BioCard
+        avatar={"avatar"}
+        name={"Emma Nasseri"}
+        email={"enasseri02@ku.edu"}
+        job={"President"}
+      ></BioCard>
+      <BioCard
+        avatar={"avatar"}
+        name={"Johnathon Lemons"}
+        email={"email address"}
+        job={"Vice President of Business"}
+      ></BioCard>
+      <BioCard
+        avatar={"avatar"}
+        name={"Hudson Headley"}
+        email={"email address"}
+        job={"Vice President of Engineering"}
+      ></BioCard>
+      <BioCard
+        avatar={"avatar"}
+        name={"Hannah Smith"}
+        email={"email address"}
+        job={"Director of Education"}
+      ></BioCard>
+      <BioCard
+        avatar={"avatar"}
+        name={"Payden Brunton"}
+        email={"email address"}
+        job={"Director of Research and Education"}
+      ></BioCard>
+      <BioCard
+        avatar={"avatar"}
+        name={"Keanen Inthavongsa"}
+        email={"email address"}
+        job={"Director of Content Creation"}
+      ></BioCard>
     </>
   );
 };
