4d6c6e7f664277f9a1ac95e3d28c37719689ed5e
hudsonhrh <hudsonhrh7@gmail.com>
2023-01-17 00:49:48 -0600

    added perry

--- webapp/src/pages/Home.tsx
+++ webapp/src/pages/Home.tsx
@@ -3,15 +3,6 @@ import jayhawk from "./jayhawk-4.png"
 import { CardsCarousel} from "../components/carousel"
 
 
-const data = [
-  {
-    image:
-      'https://images.unsplash.com/photo-1508193638397-1c4234db14d8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=80',
-    title: 'Best forests to visit in North America',
-    category: 'nature',
-  },
-  //other items
-];
 
 const Home = () => {
   return (
