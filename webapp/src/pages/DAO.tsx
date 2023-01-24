5424b98f20445cb6a86341a8c9e2424e682b2737
hudsonhrh <hudsonhrh7@gmail.com>
2023-01-24 17:34:19 -0600

    added metamask support

--- webapp/src/pages/DAO.tsx
+++ webapp/src/pages/DAO.tsx
@@ -1,10 +1,33 @@
 import React from "react";
-import Web3 from "web3";
+import { ethers } from 'ethers';
+
+var ethBalance = "5"
+
+
+
+var test = "metmask is installed"
+
+
+if(window.ethereum){
+  window.ethereum.request({method:'eth_requestAccounts'}).then((res: any)=>{console.log(res) })
+}else{
+  test = "install metamask extension or refresh with metamask launched"
+}
 
 
 
 const DAO = () => {
-  return <div>DAO test</div>;
+  return(
+  <>
+    <div>
+      DAO test
+    </div>
+    <div>
+    {test}
+    </div>
+  </>
+
+  ) 
 };
 
 export default DAO;
