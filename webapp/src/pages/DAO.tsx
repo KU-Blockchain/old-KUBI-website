430248483b81f4c99fb8786243a9febd8cecc455
hudsonhrh <hudsonhrh7@gmail.com>
2023-01-24 23:01:28 -0600

    ether js works. prints account and balance

--- webapp/src/pages/DAO.tsx
+++ webapp/src/pages/DAO.tsx
@@ -1,5 +1,7 @@
 import React from "react";
+import { useEffect, useState } from 'react';
 import { ethers } from 'ethers';
+
 
 var ethBalance = "5"
 
@@ -8,15 +10,75 @@ var ethBalance = "5"
 var test = "metmask is installed"
 
 
-if(window.ethereum){
-  window.ethereum.request({method:'eth_requestAccounts'}).then((res: any)=>{console.log(res) })
-}else{
-  test = "install metamask extension or refresh with metamask launched"
+
+
+
+let provider;
+
+if (window.ethereum) {
+  window.ethereum.request({method:'eth_requestAccounts'}).then((res: any) => {
+    console.log(res);
+    provider = new ethers.providers.Web3Provider(window.ethereum);
+    provider.getNetwork().then(network => {
+      console.log(`Connected to network: ${network.name}`);
+    });
+    provider.listAccounts().then((accounts) => {
+      console.log(`Connected accounts: ${accounts}`);
+    });
+  }).catch((err: any) => console.log(err))
+} else {
+  console.log("Web3 provider not found. Refresh Metamask");
 }
 
 
 
-const DAO = () => {
+
+
+
+interface State {
+  account: string;
+  balance: string;
+}
+
+class DAO extends React.Component<{}, State> {
+  constructor(props: {}) {
+    super(props);
+    this.state = {
+      account: "",
+      balance: "",
+    };
+  }
+
+  componentDidMount() {
+    if (window.ethereum) {
+      window.ethereum
+        .request({method:'eth_requestAccounts'})
+        .then(async (res: any) => {
+          console.log(res);
+          let provider = new ethers.providers.Web3Provider(
+            window.ethereum
+          );
+          let address = res[0];
+          this.setState({ account: address });
+          console.log(address)
+            
+          const balance = await provider.getBalance(address);
+          const balanceInEth = ethers.utils.formatEther(balance);
+          
+          console.log(balanceInEth);
+              this.setState(prevState => ({
+                ...prevState,
+                balance: balanceInEth
+              }));
+            })
+            .catch((err: any) => console.log(err));
+        }
+     else {
+      console.log("Web3 provider not found");
+    }
+  }
+
+render(){
   return(
   <>
     <div>
@@ -25,9 +87,15 @@ const DAO = () => {
     <div>
     {test}
     </div>
-  </>
-
+    <div>
+        Account: {this.state.account}
+    </div>
+    <div>
+        Balance: {this.state.balance ||0}
+    </div>
+    </>
   ) 
 };
+}
 
 export default DAO;
