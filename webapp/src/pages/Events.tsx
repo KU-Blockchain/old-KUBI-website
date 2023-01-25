6f8e4d371bcdd26d14e58b3e89cdba7880cae4e8
hudsonhrh <hudsonhrh7@gmail.com>
2023-01-24 23:46:00 -0600

    added calnder i frame to events

--- webapp/src/pages/Events.tsx
+++ webapp/src/pages/Events.tsx
@@ -6,6 +6,8 @@ const Events = () => {
     <>
       <img src={jayhawk} alt="" width ="500" height = "500" />
       <div>Events</div>
+      <iframe src="https://calendar.google.com/calendar/embed?src=ku.blockchain.institute%40gmail.com&ctz=America%2FChicago" style={{ border: 0 }} width={800} height={600} frameBorder={0} scrolling="no"></iframe>
+
     </>
   );
 };
