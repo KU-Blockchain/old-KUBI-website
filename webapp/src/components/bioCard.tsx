97cd3660e3a2add159deae45c02350d5f7d0be85
Emma Nasseri <enasseri02@gmail.com>
2023-01-27 22:15:02 -0600

    added official bio cards and formatted

--- webapp/src/components/bioCard.tsx
+++ webapp/src/components/bioCard.tsx
@@ -1,36 +1,68 @@
-import { Avatar, Text, Button, Paper } from '@mantine/core';
-import React from 'react';
-
+import { Avatar, Text, Button, Paper, HoverCard, Box } from "@mantine/core";
+import React from "react";
 
 interface IbioCardProps {
   avatar: string;
   name: string;
   email: string;
   job: string;
+  bioText: string;
 }
 
-
-export const BioCard: React.FC<IbioCardProps> = (props: IbioCardProps) =>{
+export const BioCard: React.FC<IbioCardProps> = (props: IbioCardProps) => {
   return (
-    <Paper
-      radius="md"
-      withBorder
-      p="lg"
-      sx={(theme) => ({
-        backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.white,
-      })}
-    >
-      <Avatar src={props.avatar} size={120} radius={120} mx="auto" />
-      <Text align="center" size="lg" weight={700} mt="md">
-        {props.name} 
-      </Text>
-      <Text align="center"  size="sm">
-        {props.job} • {props.email}
-      </Text>
+    <HoverCard>
+      <HoverCard.Target>
+        <Paper
+          radius="md"
+          withBorder
+          p="lg"
+          sx={(theme) => ({
+            backgroundColor:
+              theme.colorScheme === "dark" ? theme.colors.dark[8] : theme.white,
+          })}
+        >
+          <Avatar src={props.avatar} size={120} radius={120} mx="auto" />
+          <Text align="center" size="lg" weight={700} mt="md">
+            {props.name}
+          </Text>
+          <Text align="center" size="sm">
+            {props.job} • {props.email}
+          </Text>
 
-      <Button variant="default" fullWidth mt="md">
-        Send message
-      </Button>
-    </Paper>
+          <Button variant="default" fullWidth mt="md">
+            Send message
+          </Button>
+
+          
+        </Paper>
+      </HoverCard.Target>
+      <HoverCard.Dropdown>
+          <Text size="sm">
+            {props.bioText}
+          </Text>
+        </HoverCard.Dropdown>
+    </HoverCard>
   );
-}
+};
+
+/*<Box
+sx={{
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  width: 60,
+  height: 60,
+  borderRadius: 8,
+  borderWidth: 1,
+  borderColor: "#dee2e6",
+  borderStyle: "solid",
+  backgroundColor: "#fff",
+  cursor: "pointer",
+}}
+>
+<Text size="sm" weight="bold">
+  bio text
+</Text>
+</Box>
+*/
--- webapp/src/pages/About.tsx
+++ webapp/src/pages/About.tsx
@@ -3,14 +3,26 @@ import { Group, Text } from "@mantine/core";
 import { BioCard } from "../components/bioCard";
 import jayhawk from "./jayhawk-4.png";
 
+const bios = {
+  emma: "this is emma's bio",
+  hudson: "this is hudson's bio",
+  johnathon: "this is johnathon's bio",
+  hannah: "this is hannah's bio",
+  payden: "this is payden's bio",
+  keanen: "this is Keanen's bio",
+  bisshoy: "this is bisshoy's bio",
+  perry: "this is perry's bio",
+  jennifer: "This is jennifer's bio",
+};
+
 const About = () => {
   return (
     <>
       <div
         style={{
           flexDirection: "row",
-          marginLeft: 30,
-          marginRight: 30,
+          marginLeft: 50,
+          marginRight: 50,
         }}
       >
         <Text align="center" size="xl" weight={700} mt="md">
@@ -19,41 +31,71 @@ const About = () => {
         <Group position="center" spacing="lg" grow style={{ marginTop: 30 }}>
           <BioCard
             avatar={jayhawk}
+            name={"Dr. Perry Alexander"}
+            email={"email"}
+            job={"Faculty Sponsor"}
+            bioText={bios.perry}
+          ></BioCard>
+          <BioCard
+            avatar={jayhawk}
+            name={"Dr. Jennifer Lohoefener"}
+            email={"eemail"}
+            job={"Faculty Sponsor"}
+            bioText={bios.jennifer}
+          ></BioCard>
+          <BioCard
+            avatar={jayhawk}
             name={"Emma Nasseri"}
             email={"enasseri02@ku.edu"}
             job={"President"}
+            bioText={bios.emma}
           ></BioCard>
+        </Group>
+        <Group position="center" spacing="lg" grow style={{ marginTop: 30 }}>
+    
           <BioCard
             avatar={"avatar"}
             name={"Johnathon Lemons"}
             email={"email address"}
             job={"Vice President of Business"}
+            bioText={bios.johnathon}
           ></BioCard>
           <BioCard
             avatar={"avatar"}
             name={"Hudson Headley"}
             email={"hudsonh@ku.edu"}
             job={"Vice President of Engineering"}
+            bioText={bios.hudson}
           ></BioCard>
-        </Group>
-        <Group position="center" spacing="lg" grow style={{ marginTop: 30 }}>
           <BioCard
             avatar={"avatar"}
             name={"Hannah Smith"}
             email={"email address"}
             job={"Director of Education"}
+            bioText={bios.hannah}
+          ></BioCard>
+        </Group>
+        <Group position="center" spacing="lg" grow style={{ marginTop: 30 }}>
+          <BioCard
+            avatar={"avatar"}
+            name={"Bisshoy Bhattacharjee"}
+            email={"email address"}
+            job={"Executive Intern"}
+            bioText={bios.bisshoy}
           ></BioCard>
           <BioCard
             avatar={"avatar"}
             name={"Payden Brunton"}
             email={"email address"}
             job={"Director of Research and Education"}
+            bioText={bios.payden}
           ></BioCard>
           <BioCard
             avatar={"avatar"}
             name={"Keanen Inthavongsa"}
             email={"email address"}
             job={"Director of Content Creation"}
+            bioText={bios.keanen}
           ></BioCard>
         </Group>
       </div>
