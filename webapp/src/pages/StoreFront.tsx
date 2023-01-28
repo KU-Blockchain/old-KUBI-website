815b3487ee88761c833acef9cf9d6ecb4d4ff1d2
Emma Nasseri <enasseri02@gmail.com>
2023-01-28 13:33:52 -0600

    filled out storefront with a few sample products

--- webapp/src/pages/StoreFront.tsx
+++ webapp/src/pages/StoreFront.tsx
@@ -1,10 +1,11 @@
 import * as React from "react";
 import { Group, Text } from "@mantine/core";
-
+import blackTee from "./merchImages/BlackTee.png";
+import colorfulTee from "./merchImages/ColorfulTee.png";
+import waterBottle from "./merchImages/WaterBottle.png";
+import crewNeck from "./merchImages/WhiteCrewneck.png"
 import jayhawk from "./jayhawk-4.png";
-
-
-
+import { ProductCard } from "../components/productCard";
 
 const StoreFront = () => {
   return (
@@ -22,11 +23,35 @@ const StoreFront = () => {
         }}
       >
         <Text align="center" size="xl" weight={700} mt="md">
-          Meet the Team
+          Redeem Your KUBIX
         </Text>
-      
-    
-        
+
+        <Group position="center" spacing="lg" grow style={{ marginTop: 30 }}>
+          <ProductCard
+            frontImage={blackTee}
+            title={"Black Tee"}
+            price={"20,000 KUBIX"}
+            year={"2022"}
+          ></ProductCard>
+          <ProductCard
+            frontImage={colorfulTee}
+            title={"White Tee"}
+            price={"20,000 KUBIX"}
+            year={"2022"}
+          ></ProductCard>
+          <ProductCard
+            frontImage={waterBottle}
+            title={"Waterbottle"}
+            price={"20,000 KUBIX"}
+            year={"2022"}
+          ></ProductCard>
+           <ProductCard
+            frontImage={crewNeck}
+            title={"Classic White Crewneck"}
+            price={"20,000 KUBIX"}
+            year={"2022"}
+          ></ProductCard>
+        </Group>
       </div>
     </>
   );
