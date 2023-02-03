52c251c524ff1b6094291656a0418bdbbfde1148
Emma Nasseri <enasseri02@gmail.com>
2023-02-03 15:59:34 -0600

    deleted index.css

--- webapp/src/index.tsx
+++ webapp/src/index.tsx
@@ -2,7 +2,7 @@ import React from "react"
 import ReactDOM from "react-dom"
 import { BrowserRouter } from "react-router-dom"
 import App from "./pages/App"
-import "./index.css"
+
 
 //This index file just houses the App component, which houses everything else
 
